36fdefd9ea1d2b71892b981e7db285e72486c93c
Lyle Hayhurst <wlhayhurst@gmail.com>
2021-10-09 07:22:53 -0700

    Added mixed bar parquet files
    Also switched from click to typer

--- src/pdxcrime/__main__.py
+++ src/pdxcrime/__main__.py
@@ -1,12 +1,40 @@
 """Command-line interface."""
-import click
+from pathlib import Path
+
+import pyarrow as pa
+import pyarrow.parquet as pq
+import typer
+
+from pdxcrime.data.lake.crime import cleanse_and_prep_crime_data, get_crime
+from pdxcrime import __version__ as pdxcrime_version
+from pdxcrime.data.lake.real_estate import cleanse_and_prep_real_estate_data, get_real_estate
+
+app = typer.Typer()
 
 
-@click.command()
-@click.version_option()
-def main() -> None:
-    """Pdxcrime."""
+@app.command()
+def mix_parquet_files(target: str, output_path: str):
+    op = Path(output_path)
+    if not op.parent.exists():
+        raise RuntimeError(f"{op.parent} does not exist, no place to save")
+    for year in range(2015, 2022):
+        if target == "crime":
+            # see the test test_can_roundtrip_csv_to_pandas_to_arrow_to_parquet_to_arrow_to_pandas
+            crime_df = cleanse_and_prep_crime_data(year, get_crime(year))
+            parquet_file = op / f"{year}.parquet"
+            typer.echo(f"writing {parquet_file}")
+            pq.write_table(pa.Table.from_pandas(crime_df), parquet_file)
+        elif target == "real-estate":
+            re_df = cleanse_and_prep_real_estate_data(year, get_real_estate(year))
+            parquet_file = op / f"{year}.parquet"
+            typer.echo(f"writing {parquet_file}")
+            pq.write_table(pa.Table.from_pandas(re_df), parquet_file)
+
+
+@app.command()
+def version():
+    typer.echo(pdxcrime_version)
 
 
 if __name__ == "__main__":
-    main(prog_name="pdxcrime")  # pragma: no cover
+    app()
--- test/test_data_lake.py
+++ test/test_data_lake.py
@@ -1,4 +1,5 @@
 import io
+from pathlib import Path
 
 import pytest
 
@@ -6,6 +7,10 @@ from pdxcrime import get_crime
 from pdxcrime.data.lake.crime import cleanse_and_prep_crime_data, get_expected_crime_year_csv_header
 from pdxcrime.data.lake.neighborhoods import cleanse_and_prep_neighborhoods_data, get_neighborhoods
 from pdxcrime.data.lake.real_estate import cleanse_and_prep_real_estate_data, get_real_estate
+
+import pyarrow as pa
+import pyarrow.parquet as pq
+import pandas as pd
 
 
 @pytest.mark.parametrize("bad_year", [2014, 2022])
@@ -51,12 +56,30 @@ def test_cleanse_and_prep_neighborhood_data():
 
 @pytest.mark.parametrize("year", list(range(2015, 2022)))
 def test_can_join_on_neighborhoods(year):
-    crimedf = cleanse_and_prep_crime_data(year, get_crime(year))
-    redf = cleanse_and_prep_real_estate_data(year, get_real_estate(year))
+    crime_df = cleanse_and_prep_crime_data(year, get_crime(year))
+    re_df = cleanse_and_prep_real_estate_data(year, get_real_estate(year))
 
-    c, r = set(crimedf.Neighborhood.values), set(redf.Neighborhood.values)
+    c, r = set(crime_df.Neighborhood.values), set(re_df.Neighborhood.values)
 
     assert c - r == set() or c - r == {"NORTHWEST INDUSTRIAL"}, c - r
+
+    # we do not care about Dunthorpe https://en.wikipedia.org/wiki/Dunthorpe,_Oregon
     assert (
         r - c == set() or r - c == {"DUNTHORPE"} or r - c == {"MULT CO RIVERDALE AREA (DUNTHORPE)"}
     ), (r - c)
+
+
+def test_can_roundtrip_csv_to_pandas_to_arrow_to_parquet_to_arrow_to_pandas(tmp_path: Path):
+    year = 2020
+    crime_df: pd.DataFrame = cleanse_and_prep_crime_data(year, get_crime(year))
+    table: pa.Table = pa.Table.from_pandas(crime_df)
+    parquet_file = tmp_path / "2020.parquet"
+    pq.write_table(table, parquet_file)
+
+    vtable: pa.Table = pq.read_table(parquet_file)
+    crime_df_2 = vtable.to_pandas()
+    assert crime_df_2.equals(crime_df)
+
+
+# def test_crime_levels_in_dunthorpe():
+#
