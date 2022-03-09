8f3bb585eae2d3c0d7c72aa6c2d3e342afef585b
Lyle Hayhurst <wlhayhurst@gmail.com>
2022-03-09 05:52:48 -0800

    added csv cutter, summary csv files

--- src/pdxcrime/__main__.py
+++ src/pdxcrime/__main__.py
@@ -1,9 +1,9 @@
 """Command-line interface."""
 from pathlib import Path
 
+import pandas as pd
 import pyarrow as pa
 import pyarrow.parquet as pq
-
 import typer
 
 from pdxcrime import __version__ as pdxcrime_version
@@ -30,6 +30,36 @@ def mix_parquet_files(target: str, output_path: str) -> None:
             parquet_file = op / f"{year}.parquet"
             typer.echo(f"writing {parquet_file}")
             pq.write_table(pa.Table.from_pandas(re_df), str(parquet_file))
+    return
+
+
+@app.command()
+def mix_csv_files(output_path: str) -> None:
+    op = Path(output_path)
+    if not op.parent.exists():
+        raise RuntimeError(f"{op.parent} does not exist, no place to save")
+
+    crime_dfs = []
+    real_estate_dfs = []
+
+    typer.echo("Cleaning data for 2015-2022")
+    for year in range(2015, 2022):
+        crime_df = cleanse_and_prep_crime_data(year, get_crime(year))
+        crime_dfs.append(crime_df)
+        re_df = cleanse_and_prep_real_estate_data(year, get_real_estate(year))
+        real_estate_dfs.append(re_df)
+
+    crime_df: pd.DataFrame = pd.concat(crime_dfs)
+
+    crime_csv = op / "pdx_crime_2015_2022.csv"
+    typer.echo(f"Cutting {crime_csv}")
+    crime_df.to_csv(str(crime_csv), index=False)
+
+    real_estate_df = pd.concat(real_estate_dfs)
+    real_estate_csv = op / "pdx_real_estate_2015_2022.csv"
+    typer.echo(f"Cutting {real_estate_csv}")
+    real_estate_df.to_csv(str(real_estate_csv), index=False)
+
     return
 
 
